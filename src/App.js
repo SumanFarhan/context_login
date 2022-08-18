2754fa9d84002608c56ecc578f6de53e41b553cf
SumanFarhan <sumanfarhan2800@gmail.com>
2022-08-18 16:46:52 +0500

    login/Frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import 'antd/dist/antd.css'
+import Left_side from './Left_side'
+import Right_side from './Right_side'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className="flex-container">
+        <div className="left_side"><Left_side/></div>
+        <div className="right_side"><Right_side/></div>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/Left_side.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Slider from "./Slider";
+
+const Left_side = () => {
+  return (
+    <>
+      <div className="main_left">
+          <Slider/>
+      </div>
+    </>
+  );
+};
+
+export default Left_side;
